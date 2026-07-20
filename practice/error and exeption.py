d221730b9becd17fea0e4aa3d60a43f2859e068b
Shatakshi Dubey <shatakshid33@gmail.com>
2026-07-20 22:57:37 +0530

    practice

---
+++ practice/error and exeption.py
@@ -0,0 +1,49 @@
+#practicing handling error
+#for i in [1,2,3]:
+ #   try:
+  #      j=i**2
+   #     print(j)
+    #except:
+     #   print('oops its a string not int')
+      #  continue
+ #   finally:
+  #      print('operation occured')
+        
+#practicing handling error
+#for i in [1,2,3]:
+ #   try:
+  #      i**2
+   #     print(i**2)
+    #except:
+     #   print('oops its a string not int')
+      #  continue
+   # finally:
+    #    print('operation occured')
+
+
+
+#practicing handling error
+#for i in ['a','b','c']:
+ #   try:
+  #      i**2
+   #     print(i**2)
+    #except:
+     #   print('oops its a string not int')
+      #  continue
+    #finally:
+     #   print('operation occured')
+
+
+#practicing handling error
+#for i in ['a','b','c', 3, 4]:
+ #   try:
+  #      i**2
+   #     print(i**2)
+    #except:
+     #   print('oops its a string not int')
+      #  continue
+   # finally:
+    #    print('operation occured')
+
+
+#ques 2
